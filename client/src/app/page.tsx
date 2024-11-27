ecbf87934cb1e84f90f47986119b732e467ef6f9
tarun-jaikishan <tarunjaikishanpk@gmail.com>
2024-11-27 13:13:17 +0530

    hide scrollbar added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,11 @@ import SideBar from "@/components/SideBar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <div className="w-1/5">
+    <div className="flex">
+      <div className="w-1/5 sticky top-0 h-screen overflow-y-auto hide-scrollbar">
         <SideBar />
       </div>
-      <div className="w-full"></div>
+      <div className="w-4/5 overflow-y-auto">asad</div>
     </div>
   );
 }
