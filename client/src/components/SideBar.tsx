c60c2d59afe69ed395341daf93235aa501131428
tarun-jaikishan <tarunjaikishanpk@gmail.com>
2024-11-27 13:43:22 +0530

    sidebar added

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,45 +1,51 @@
 import { CiSearch } from "react-icons/ci";
+import { IoMdPersonAdd } from "react-icons/io";
 
 import ContactPicker from "./common/ContactPicker";
 
 export default function SideBar() {
   return (
-    <aside className="flex flex-col justify-start items-start p-5 space-y-2">
-      <div className="flex w-full">
-        <div className="flex items-center justify-center bg-purple-200 pl-2 py-2 rounded-l-lg">
-          <CiSearch className="text-2xl" />
+    <div className="flex flex-col justify-start items-start p-5 space-y-2">
+      <div className="flex gap-2">
+        <button
+          type="button"
+          className="text-white bg-purple-400 px-3 rounded-lg hover:bg-purple-500 duration-300"
+          title="Add Contact"
+        >
+          <IoMdPersonAdd className="text-xl" />
+        </button>
+        <div className="flex w-full">
+          <div className="flex items-center justify-center bg-purple-200 pl-2 py-2 rounded-l-lg">
+            <CiSearch className="text-2xl" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-purple-200 px-2 py-2 rounded-r-lg text-black focus:outline-none w-full"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-purple-200 px-2 py-2 rounded-r-lg text-black focus:outline-none w-full"
-        />
       </div>
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-      <ContactPicker />
-    </aside>
+
+      <ContactPicker
+        isActive={true}
+        name="Tarun Jaikishan"
+        recentChat="1234567890"
+        chatCount={0}
+      />
+
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, i) => {
+        return (
+          <ContactPicker
+            name="Test 1"
+            recentChat="  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut laboriosam
+          eligendi, quibusdam minima facilis laudantium, deserunt ab beatae vero
+          quia aliquid voluptatibus numquam obcaecati magni similique labore!
+          Sequi, repudiandae rerum?"
+            chatCount={200}
+            key={i}
+          />
+        );
+      })}
+    </div>
   );
 }
--- client/src/components/common/ContactPicker.tsx
+++ client/src/components/common/ContactPicker.tsx
@@ -5,29 +5,55 @@ import profile from "@/assets/profile.jpg";
 
 type props = {
   className?: string;
+  isActive?: boolean;
+  name: string;
+  recentChat: string;
+  chatCount: number;
 };
 
-export default function ContactPicker({ className = "" }: props) {
+export default function ContactPicker({
+  className = "",
+  isActive = false,
+  name,
+  recentChat,
+  chatCount,
+}: props) {
+  const isOneDigit = chatCount < 10;
+
   return (
     <button
       type="button"
       className={twMerge(
-        "flex items-center hover:bg-slate-200 duration-300 py-3 pl-3 text-start rounded-lg w-full",
+        `flex items-center hover:bg-slate-200 duration-300 py-3 pl-3 text-start rounded-lg w-full ${
+          isActive ? "bg-slate-200" : ""
+        }`,
         className
       )}
     >
       <Image src={profile} alt="profile" className="w-16 rounded-lg" />
       <div className="pl-3">
         <div className="flex items-center justify-between">
-          <h3 className="font-semibold">Tarun Jaikishan</h3>
+          <h3 className="font-semibold">{name}</h3>
           <h5 className="text-slate-400 text-sm">5m</h5>
         </div>
-        <p className="text-sm min-w-[14rem] max-w-[14rem] whitespace-nowrap overflow-hidden text-ellipsis">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut laboriosam
-          eligendi, quibusdam minima facilis laudantium, deserunt ab beatae vero
-          quia aliquid voluptatibus numquam obcaecati magni similique labore!
-          Sequi, repudiandae rerum?
-        </p>
+        <div className="flex items-center justify-between">
+          <p
+            className={`text-sm min-w-[14rem] max-w-[14rem] whitespace-nowrap overflow-hidden text-ellipsis text-slate-500 ${
+              isActive ? "text-purple-400" : ""
+            }`}
+          >
+            {recentChat}
+          </p>
+          {chatCount > 0 && (
+            <div
+              className={`${
+                isOneDigit ? "w-6 h-6" : "w-6 h-6 px-1.5"
+              } bg-purple-400 rounded-full text-white text-xs flex items-center justify-center`}
+            >
+              {chatCount}
+            </div>
+          )}
+        </div>
       </div>
     </button>
   );
